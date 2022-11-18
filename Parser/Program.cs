761ef04c12a5f14bdf02dd860d57c80af1b9bf23
Андрей Углик <A.Uglik@cbt.by>
2022-11-18 19:01:02 +0300

    appsettings

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -1,19 +1,47 @@
-﻿using Parser.BL.Data.Interfaces;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Parser.BL.Data.Interfaces;
 using Parser.BL.Data.Services;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Parser
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            IParserService xx = new ApiParserService();
+            var services = ConfigureServices();
 
-            var xxc = await xx.GetProductsAsync("sd");
+            Work(services);
 
             Console.ReadKey();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables().Build();
+
+            var services = new ServiceCollection()
+                .AddSingleton<IConfiguration>(configuration)
+                .AddLogging();
+
+            services.AddTransient<IParserService, ApiParserService>();
+
+            return services;
+        }
+
+        private static void Work(IServiceCollection services)
+        {
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var parser = scope.ServiceProvider.GetService<IParserService>();
+
+            var xxc = parser.GetProductsAsync("sd").GetAwaiter().GetResult();
         }
     }
 }
